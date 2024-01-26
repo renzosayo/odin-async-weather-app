85677931f13a94ae585451d7a1c46d42ce9c9622
renzosayo* <71843954+renzosayo@users.noreply.github.com>
2024-01-26 14:15:27 +0800

    Complete app without styling

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ async function getData (location) {
     return await weatherData.json();
 }
 
-getData('manila').then((data) => console.log(data));
+// only get condition and temp in C
+function filterData (data) {
+    const temp_c = data.current.temp_c;
+    const last_updated = data.current.last_updated;
+    return { temp_c, last_updated, ...data.current.condition };
+}
+
+function displayResult (data) {
+    const weatherIcon = document.querySelector('.weather-icon');
+    const weatherDesc = document.querySelector('.weather-desc');
+    const temp = document.querySelector('.temp');
+    const date = document.querySelector('.date');
+    weatherIcon.src = `https:${data.icon}`;
+    weatherDesc.textContent = data.text;
+    temp.textContent = `${data.temp_c}° C`;
+    date.textContent = data.last_updated;
+}
+
+function start() {
+    const submit = document.querySelector('.submit');
+    submit.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const location = document.querySelector('#location').value;
+        const filtered = filterData(await getData(location).catch((err) => console.log(err)));
+        displayResult(filtered);
+    });
+};
+
+start();
